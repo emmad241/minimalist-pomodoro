b28b04d5aa848472cb12a25a13ffd7c0134e4ec9
emmad241 <emmaduffy241@gmail.com>
2022-06-17 03:22:07 +0100

    Reset timer at end of break time

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const gong = 'http://www.buddhadhammasangha.com/SecondLevelSite/ThirdLevelSite/A
 const sound = new Audio(gong);
 
 startTimerBtn.addEventListener("click", function () {
-	startTimer(1500, 300, counter);
+	startTimer(5, 5, counter);
 });
 
 stopTimerBtn.addEventListener("click", function () {
@@ -42,7 +42,11 @@ function startTimer(workTime, playTime, timer){
 			}else{
 				timer.innerHTML = 'Break time is over!';
 				sound.play();
-				clearInterval(countdown);
+				
+				setTimeout(function() {
+					stopTimer(timer);
+				}, 4000);
+
 				return;
 			}
 		}
